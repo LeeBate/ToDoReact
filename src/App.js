d6b49bca4b48fd8aa001f4f2fa0b35f68b50411a
LeeBate <harukaxd02@gmail.com>
2021-11-26 21:22:17 +0700

    ToDo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Todo from './components/todo';
+function App() {
+  
+  return (
+  <div className="App">
+    <header>
+      <h1 style={{ fontSize: "2cm", fontWeight: "600", marginBottom: "2cm",
+          lineHeight: "1cm", color: "Blue",  textAlign: "center" }}>ToDoReactNative
+      </h1>
+    </header>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/todo.js
@@ -0,0 +1,13 @@
+import React from "react"
+function Todo() {
+
+    return (
+      <header>
+      <h1 style={{ fontSize: "2cm", fontWeight: "600", marginBottom: "2cm",
+       lineHeight: "1cm", color: "Blue",  textAlign: "center" }}>ToDoReactNative
+      </h1>
+    </header>
+    );
+  }
+
+export default Todo
